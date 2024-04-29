46805f3e0b43fd7ff2e1d95c5b243fc1ababc0bf
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-29 13:25:56 +0200

    ejer03.php creado

---
+++ e3/ejer03.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $signo = '';
+    echo  '<p>Hola <span style="font-weight:bold">' . $_GET['nombre'] . '</span></p>';
+    echo '<p>Tu horoscopo es</p>';
+
+    switch ($_GET['mes']) {
+        case 1:
+            $signo = ($_GET['dia'] > 19 ? 'acuario' : 'capricornio');
+            break;
+        case 2:
+            $signo = ($_GET['dia'] > 19 ? 'piscis' : 'acuario');
+            break;
+        case 3:
+            $signo = ($_GET['dia'] > 20 ? 'aries' : 'piscis');
+            break;
+        case 4:
+            $signo =  ($_GET['dia'] > 19 ? 'tauro' : 'aries');
+            break;
+        case 5:
+            $signo =  ($_GET['dia'] > 20 ? 'geminis' : 'tauro');
+            break;
+        case 6:
+            $signo =  ($_GET['dia'] > 20 ? 'cancer' : 'geminis');
+            break;
+        case 7:
+            $signo =  ($_GET['dia'] > 22 ? 'leo' : 'cancer');
+            break;
+        case 8:
+            $signo =  ($_GET['dia'] > 22 ? 'virgo' : 'leo');
+            break;
+        case 9:
+            $signo =  ($_GET['dia'] > 22 ? 'libra' : 'virgo');
+            break;
+        case 10:
+            $signo =  ($_GET['dia'] > 22 ? 'escorpio' : 'libra');
+            break;
+        case 11:
+            $signo =  ($_GET['dia'] > 21 ? 'sagitario' : 'escorpio');
+            break;
+        case 12:
+            $signo =  ($_GET['dia'] > 21 ? 'capricornio' : 'sagitario');
+            break;
+    }
+    echo '<img src="img/signo-' . $signo . '.gif"/>';
+    echo '<p>' . ucfirst($signo) . '</p>';
+    echo '<p>' . $_GET['dia'] . '/' . $_GET['mes'] . '</p>';
+    echo '<p><a href="ejer03.html">Volver</a>';
+
+    ?>
+</body>
+
+</html>
