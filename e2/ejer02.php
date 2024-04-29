3b4fab96a5176914ce8a578fec5f569597ca52c2
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-29 12:13:12 +0200

    ejer02.php creado

---
+++ e2/ejer02.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style type='text/css'>
+        table,
+        th {
+            border: solid black 1px;
+            border-collapse: collapse;
+            text-align: center;
+            padding: 5px;
+        }
+
+
+        td {
+            border-right: solid black 1px;
+            padding: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $anio = 1960;
+    echo '<table>';
+    echo '<thead><th colspan=10><p>HOROSCOPO CHINO</p><p>Selecciona tu año de nacimiento</p></th></thead>';
+    echo '<tbody>';
+    for ($i = 0; $i < 7; $i++) {
+        echo '<tr>';
+        for ($j = 0; $j < 10; $j++) {
+            echo '<td><a href="ejer02b.php?anio=' . $anio . '>' . $anio . '</a></td>';
+            $anio++;
+        }
+        echo '</tr>';
+    }
+    echo '</tbody>';
+    echo '</table>';
+
+
+    ?>
+</body>
+
+</html>
