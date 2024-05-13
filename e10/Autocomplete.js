2358c354ae1f9dc8c5c4828b20d3a4d3ba4d1ee9
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-13 15:32:30 +0200

    colorear caracteres que coincidan con la búsqueda

--- e10/Autocomplete.js
+++ e10/Autocomplete.js
@@ -1,4 +1,4 @@
-var valores_provisionales = ['patata', 'cebo', 'peine', 'ceja', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra', 'palabra'];
+
 var valores = [];
 var cantidad_valores = 10;
 
@@ -24,7 +24,7 @@ function rellenarTabla() {
     valores.forEach(valor => {
 
         let opcion = $('<tr class="p-1 opcion">');
-        $(opcion).on('click', (e) => { mostrarPalabra($(e.target.firstChild).text()) });
+        $(opcion).on('click', (e) => { mostrarPalabra($(e.currentTarget.firstChild).text()) });
         $(opcion).append(`<td>${valor.valor}</td><td class="text-end text-secondary fw-light">${valor.id}</td>`);
         $('#resultados').append(opcion);
 
@@ -32,7 +32,7 @@ function rellenarTabla() {
 }
 function autocompletar(busqueda) {
     var resultados = 0;
-
+    var caracteres = busqueda.split('');
     $('.opcion').each((index, valor) => {
         $(valor.firstChild).html((index, palabra) => (palabra.replace(new RegExp('<(.*?)>', 'g'), '')));
         if ($('#modo').is(':checked')) {
@@ -42,20 +42,20 @@ function autocompletar(busqueda) {
             } else if (busqueda != '') {
                 $(valor).removeClass('d-none');
                 $(valor.firstChild).html((index, palabra) => {
-                    reemplazo = palabra.substring(palabra.toUpperCase().indexOf(busqueda.toUpperCase()), );
-                    palabra = palabra.split(new RegExp(busqueda, 'i'), 2);
-                    return palabra.join(`<strong>${reemplazo}</strong>`)
+                    let ultimoIndex = palabra.toUpperCase().indexOf(busqueda.toUpperCase());
+                    return colorearTexto(palabra, caracteres, ultimoIndex, true);
                 });
                 resultados++;
             }
         } else {
-            let caracteres = busqueda.split('');
+
             let ultimoIndex = 0;
             let coincidencias = 0;
             caracteres.forEach(caracter => {
                 if ($(valor.firstChild).text().toUpperCase().includes(caracter.toUpperCase(), ultimoIndex)) {
                     coincidencias++;
-                    ultimoIndex = $(valor.firstChild).text().toUpperCase().indexOf(caracter.toUpperCase());
+                    ultimoIndex = $(valor.firstChild).text().toUpperCase().indexOf(caracter.toUpperCase(), ultimoIndex);
+
                 }
             })
             if (coincidencias != caracteres.length) {
@@ -63,6 +63,11 @@ function autocompletar(busqueda) {
             } else {
                 $(valor).removeClass('d-none');
                 resultados++;
+                ultimoIndex = 0;
+                $(valor.firstChild).html((index, palabra) => {
+                    return colorearTexto(palabra, caracteres, ultimoIndex, false);
+
+                });
             }
         }
 
@@ -72,7 +77,7 @@ function autocompletar(busqueda) {
         esconderTabla('')
     } else {
 
-        $('#cantidad').html(resultados > 0 ? `Se han encontrado <strong>${resultados}</strong> resultados` : `Se han encontrado <strong>0</strong> resultados para <strong>"${busqueda}"</strong>`)
+        $('#cantidad').html(resultados > 0 ? `Se ${resultados > 1 ? 'han' : 'ha'} encontrado <span class="fw-bold">${resultados}</span> ${resultados > 1 ? 'resultados' : 'resultado'}` : `Se han encontrado <span class="fw-bold">0</span> resultados para <span class="fw-bold">"${busqueda}"</span>`)
         mostrarTabla();
     }
 
@@ -90,10 +95,27 @@ function mostrarTabla() {
     $('#autocompletar').removeClass('d-none');
 }
 
+function colorearTexto(palabra, caracteres, ultimoIndex, estricto) {
+    palabra = palabra.split('');
+    caracteres.forEach(caracter => {
+        if (estricto) {
+            var reemplazo = palabra.findIndex((elemento, index) => elemento.toUpperCase() == caracter.toUpperCase() && (index == ultimoIndex || index == ultimoIndex + 1))
+        } else {
+            var reemplazo = palabra.findIndex((elemento, index) => elemento.toUpperCase() == caracter.toUpperCase() && index >= ultimoIndex)
+        }
+        palabra[reemplazo] = `<span class="text-danger">${palabra[reemplazo]}</span>`;
+        ultimoIndex = reemplazo;
+    })
+
+    return palabra.join('');
+}
+
 function mostrarPalabra(palabra) {
     $('#busqueda').val(palabra)
     $('#encontrado').text(palabra)
     esconderTabla('')
 }
+
+
 
 $(window).on('load', cargarPagina);
