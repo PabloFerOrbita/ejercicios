1142cac9b4e8f6c15520dfaae65f3cd4619dd465
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-14 18:24:13 +0200

    ejercicio 32

---
+++ e32/PosicionPares.js
@@ -0,0 +1,14 @@
+function cargarPagina() {
+    document.querySelector('button').addEventListener('click', () => { alert(posiciones(document.querySelector('input').value)) })
+}
+
+function posiciones(array) {
+    arrayPares = [];
+    array.split(' ').forEach((numero, posicion) => {
+        numero % 2 == 0 && arrayPares.push(posicion);
+    })
+
+    return arrayPares;
+}
+
+window.addEventListener('click', cargarPagina)
