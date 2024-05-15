fc4d9bcef142a988322e3de136671b00422aa5d3
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-15 10:50:43 +0200

    ejercicio 43

---
+++ e43/codigo.js
@@ -0,0 +1,5 @@
+var meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'ocutbre', 'noviembre', 'diciembre'];
+
+meses.forEach(mes => {
+    console.log(mes);
+})
