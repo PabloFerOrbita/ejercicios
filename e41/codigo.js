00c542523d15b4a13417857a338f4ff4efbc42eb
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-15 10:33:30 +0200

    ejercicio 41

---
+++ e41/codigo.js
@@ -0,0 +1,5 @@
+
+ //Imprime en la consola las palabras "Hola Mundo"
+  console.log("Hola Mundo!");
+  //imprime en consola las palabras "Soy el primer script"
+  console.log("Soy el primer script");
