d89462450aece77d28e00e8f111450b688426216
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-15 10:39:15 +0200

    ejercicio 42

---
+++ e42/codigo.js
@@ -0,0 +1,5 @@
+ mensaje = "Hola mundo!\n Que facil es incluir 'comillas simples'\n y \"comillas dobles\""
+ //muestra el mensaje almacenado con una alerta
+ alert(mensaje);
+ //imprime en consola las palabras "Soy el primer script"
+ console.log("Soy el primer script");
