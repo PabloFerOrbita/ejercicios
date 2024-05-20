4f349a7e8338e5d28070be8edbffb07277b75085
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-20 10:58:22 +0200

    ejercicio 58

--- e58/Ojos.js
+++ e58/Ojos.js
@@ -2,7 +2,11 @@ function cargarPagina() {
     $(document).on('mousemove', movimiento);
 }
 function movimiento(e) {
-    var x= $(e).offset().left + ($('#ojo').width() / 2);
+    var x = $('.ojo').offset().left + ($('.ojo').width());
+    var y = $('.ojo').offset().top + ($('.ojo').height());
+    var radianes = Math.atan2(e.pageX - x, e.pageY - y);
+    var rotacion = (radianes * (180 / Math.PI) * (-1)) + 180;
+    $('.ojo').css({ transform: `rotate(${rotacion}deg)` });
 
 }
 
