e7835506a3f0bc72e1c2c31354d134b2fd4f28f5
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-27 09:30:52 +0200

    ejercicio 18 php

---
+++ ejercicios_solo_php/e18_php/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="SumarRango.js"></script>
+</head>
+
+<body>
+    <form action="" method="get">
+        <input type="number" id="numero1" name="numero1" />
+        <input type="number" id="numero2" name="numero2" />
+        <button>Sumar Rango</button><br>
+    </form>
+    <?php
+    if (isset($_GET['numero1']) && isset($_GET['numero2'])) {
+        echo sumarRango($_GET['numero1'], $_GET['numero2']);
+    }
+    function sumarRango($numero1, $numero2)
+    {
+        $suma = 0;
+        for ($i = $numero1; $i <= $numero2; $i++) {
+            $suma += $i;
+        }
+        return $suma;
+    }
+
+    ?>
+</body>
+
+</html>
