73aa8126adee53a4f3b4c40ce297807f124fbed4
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-05-27 10:01:12 +0200

    ejercicio 22 php

--- ejercicios_solo_php/e22_php/index.php
+++ ejercicios_solo_php/e22_php/index.php
@@ -5,25 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="MultiplicarArreglo.js"></script>
 </head>
 
 <body>
     <form action="" method="get">
         <input type="text" id="array" name="array" /> <br>
-        <button> Sumar Array</button><br>
+        <button>Multiplicar Array</button><br>
     </form>
     <?php
     if (isset($_GET['array'])) {
-        echo sumarArreglo($_GET['array']);
+        echo multiplicarArreglo($_GET['array']);
     }
-    function sumarArreglo($array)
+    function multiplicarArreglo($array)
     {
-        $suma = 0;
+        $producto = 1;
         $array = explode(' ', $array);
         foreach ($array as $numero) {
-            $suma += intval($numero);
+            $producto *= intval($numero);
         }
-        return $suma;
+        return $producto;
     }
     ?>
 </body>
